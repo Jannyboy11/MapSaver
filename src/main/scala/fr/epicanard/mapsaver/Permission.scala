c686c054a07b3a392a8fc7f82eb380b8cb952b68
EpiCanard <epicanard@gmail.com>
2021-09-07 00:20:20 +0200

    Add permission model

---
+++ src/main/scala/fr/epicanard/mapsaver/Permission.scala
@@ -0,0 +1,36 @@
+package fr.epicanard.mapsaver
+
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+
+sealed abstract class Permission(permission: String) {
+  def isSetOn(player: Player): Boolean = player != null && player.hasPermission(permission)
+
+  def isSetOn(sender: CommandSender, defaultSender: Boolean): Boolean =
+    sender match {
+      case player: Player => isSetOn(player)
+      case _              => defaultSender
+    }
+
+  def isSetOn(sender: CommandSender): Boolean = isSetOn(sender, defaultSender = true)
+}
+
+object Permission {
+  case object SaveMap extends Permission("mapsaver.commands.save")
+
+  case object UpdateMap extends Permission("mapsaver.commands.update")
+
+  case object ImportMap extends Permission("mapsaver.commands.import")
+
+  case object ListMap extends Permission("mapsaver.commands.list")
+
+  case object InfoMap extends Permission("mapsaver.commands.info")
+
+  case object AdminReload extends Permission("mapsaver.admin.commands.reload")
+
+  case object AdminImportMap extends Permission("mapsaver.admin.commands.import")
+
+  case object AdminListMap extends Permission("mapsaver.admin.commands.list")
+
+  case object AdminInfoMap extends Permission("mapsaver.admin.commands.info")
+}
