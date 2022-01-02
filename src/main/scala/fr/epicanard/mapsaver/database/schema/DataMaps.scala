054756233d21b9c7f9217a3dab96d10509ef0977
EpiCanard <epicanard@gmail.com>
2022-01-02 23:39:41 +0100

    Fix created_at always updated

--- src/main/scala/fr/epicanard/mapsaver/database/schema/DataMaps.scala
+++ src/main/scala/fr/epicanard/mapsaver/database/schema/DataMaps.scala
@@ -10,7 +10,7 @@ class DataMaps(tag: Tag) extends Table[DataMap](tag, "data_maps") {
   def id    = column[Int]("id", O.PrimaryKey, O.AutoInc)
   def bytes = column[Array[Byte]]("bytes", SqlType("VARBINARY(16384)"))
   def createdAt =
-    column[Timestamp]("created_at", SqlType("DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"))
+    column[Timestamp]("created_at", SqlType("DATETIME DEFAULT CURRENT_TIMESTAMP"))
   def updatedAt =
     column[Timestamp]("updated_at", SqlType("DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP"))
 
